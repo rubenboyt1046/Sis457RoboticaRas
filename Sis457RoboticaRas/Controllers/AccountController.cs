e8d163d246f9598dce317bc1760f267f3f3e9d3d
Ruben Arteaga <rubentec3@gmail.com>
2024-01-21 00:01:46 -0400

    agregando cuentas de usuario

--- Sis457RoboticaRas/Controllers/AccountController.cs
+++ Sis457RoboticaRas/Controllers/AccountController.cs
@@ -1,11 +1,91 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Sis457RoboticaRas.Models;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 
 namespace Sis457RoboticaRas.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
-       
+        private readonly RoboticaRasContext _context;
+
+        public AccountController(RoboticaRasContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Intentos de inicio de sesión no válidos.");
+                return View(model);
+            }
+
+            var usuario = _context.Usuarios.Include(e => e.IdEmpleadoNavigation)
+                .Where(x => x.Estado == 1 && x.Usuario1 == model.usuario &&
+                    x.Clave == Util.Encrypt(model.clave)).FirstOrDefault();
+            if (usuario != null)
+            {
+                TempData["isLogged"] = true;
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, usuario.Usuario1),
+                    new Claim("FullName", $"{usuario.IdEmpleadoNavigation.Nombre} {usuario.IdEmpleadoNavigation.Apellidos}"),
+                    new Claim(ClaimTypes.Email, "asrrobotics@gmail.com"),
+                    new Claim(ClaimTypes.Role, usuario.IdEmpleadoNavigation.Cargo, ClaimValueTypes.String)
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(15),
+                    IsPersistent = model.recordarme
+                };
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties);
+
+                if (returnUrl == null) returnUrl = ViewData["ReturnUrl"]?.ToString();
+                if (returnUrl != null) return Redirect(returnUrl);
+                else return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+            else
+            {
+                ViewBag.ReturnUrl = returnUrl;
+                ModelState.AddModelError("", "Intentos de inicio de sesión no válidos.");
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            TempData["isLogged"] = false;
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
     }
 }
--- Sis457RoboticaRas/Program.cs
+++ Sis457RoboticaRas/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Sis457RoboticaRas.Models;
 
@@ -8,6 +9,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<RoboticaRasContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = new PathString("/Account/Login"));
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -21,6 +27,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCookiePolicy();
+app.UseSession();
+app.UseAuthentication();
 
 app.UseRouting();
 
