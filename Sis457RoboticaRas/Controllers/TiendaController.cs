86e1c987a3d6ae51c18c307533d8723ff44d48e6
Ruben Arteaga <rubentec3@gmail.com>
2024-01-24 18:01:43 -0400

    mejoras ultimas

---
+++ Sis457RoboticaRas/Controllers/TiendaController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sis457RoboticaRas.Models;
+
+namespace Sis457RoboticaRas.Controllers
+{
+    public class TiendaController : Controller
+
+    {
+        private readonly RoboticaRasContext _context;
+
+        public TiendaController(RoboticaRasContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var listaA = _context.Venta.ToList();
+            var listaB = _context.VentaDetalles.ToList();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Tienda tienda)
+        {
+            if (ModelState.IsValid)
+            {
+                var Venta = new Ventum
+                {
+
+                    IdUsuario = tienda.IdUsuario,
+                    IdCliente = tienda.IdCliente,
+                    TotalVenta = tienda.TotalVenta,
+                    UsuarioRegistro = User.Identity?.Name,
+                    FechaVenta = DateTime.Now,
+                    Estado = 1
+                    
+                };
+                _context.Venta.Add(Venta);
+                _context.SaveChanges();
+
+                var VentaDetalle = new VentaDetalle
+                {
+                    IdVenta = Venta.Id,
+                    IdProducto = tienda.IdProducto,
+                    Cantidad = tienda.Cantidad,
+                    PrecioUnitario = tienda.PrecioUnitario,
+                    Total = tienda.Total,
+                    UsuarioRegistro = User.Identity?.Name,
+                    FechaRegistro = DateTime.Now,
+                    Estado = 1
+                };
+                _context.VentaDetalles.Add(VentaDetalle);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(tienda);
+        }
+
+
+    }
+}
---
+++ Sis457RoboticaRas/Models/Tienda.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sis457RoboticaRas.Models;
+
+public partial class Tienda
+{
+    public int IdUsuario { get; set; }
+
+    public int IdCliente { get; set; }
+
+    public decimal TotalVenta { get; set; }
+
+    public DateTime FechaVenta { get; set; }
+    public int IdVenta { get; set; }
+
+    public int IdProducto { get; set; }
+    public int Cantidad { get; set; }
+
+    public decimal PrecioUnitario { get; set; }
+
+    public decimal Total { get; set; }
+
+    public string UsuarioRegistro { get; set; } = null!;
+
+    public DateTime FechaRegistro { get; set; }
+
+    public short Estado { get; set; }
+
+    public virtual Cliente IdClienteNavigation { get; set; } = null!;
+
+    public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+
+
+}
